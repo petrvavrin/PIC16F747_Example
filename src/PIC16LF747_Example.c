d6707ea5500f03eb33b6c68f6b59a49ac78668b4
petrvavrin <148798764+petrvavrin@users.noreply.github.com>
2023-11-02 18:14:51 +0100

    Update PIC16LF747_Example.c

--- src/PIC16LF747_Example.c
+++ src/PIC16LF747_Example.c
@@ -45,21 +45,6 @@
 #define RE2 PORTE.F2 //
 #define RE3 PORTE.F3 //
 //---------------------
-
-// Nokia 5110 LCD module connections - use software SPI, Tato deklarace musi byt jako prvni
-#define LCD_RST  RE2_bit  // reset pin, optional!
-#define LCD_CS   RE1_bit  // chip select pin, optional!
-#define LCD_DC   RE0_bit  // data/command pin
-#define LCD_DAT  RA5_bit  // data-in pin (MOSI)
-#define LCD_CLK  RA4_bit  // clock pin
-
-#include <NOKIA5110.h>    // include Nokia 5110 LCD driver source file, Tato deklarace musi byt jako druha
-
-/*const uint8_t logo16_glcd_bmp[] = { // declare bmp image located in PIC ROM
-  48, 112, 240, 240,  96,  96, 248, 158, 255, 127, 120, 224,
-  224, 192, 192, 192,  0,  96, 120, 125,  63,  59,  29,  31,
-  61, 123, 255, 243,  1,   1,   0,   0
-  };*/
   
 //-----------------------------------------------------------------------------
 void InitHW(){
@@ -97,20 +82,7 @@ void LED_Blik() {
 //--------------------------------------------------------------------------------------
 void main() {
     InitHW();
-    LCD_Begin();                                  // initialize the LCD
-    LCD_SetContrast(120);                          // set LCD contrast
-    LCD_Fill();                                   // test LCD pixels
-    LCD_Display();                                // show splashscreen (kyticka logo)
-    Delay_ms(500);
-    LCD_Clear();                                  // clear the display buffer
-
-    LCD_TextSize(1);                              // set text size to 1
-    LCD_TextColor(BLACK, WHITE);                  // set text color to black with white background
-    LCD_GotoXY(7, 3);                             // move cursor to position
-    LCD_Print("Teplota C");
-    LCD_DrawRect(51, 3, 3, 3, BLACK);             // print degree symbol ( ° )
-    LCD_DrawRoundRect(0, 0, 84, 48, 7, BLACK);    // obly ramecek
-    LCD_Display();
+    
     //------------------------- L O O P -----------------------------
     while (1) {
 
